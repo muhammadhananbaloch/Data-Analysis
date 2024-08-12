2e7df2607fe2a48f146dc2a885b9ff810d3a7cc1
muhammadhananbaloch <106446229+muhammadhananbaloch@users.noreply.github.com>
2024-08-12 12:55:31 +0500

    Scrap the product names and id.

--- Web Scraping/practice webscrape/jj.py
+++ Web Scraping/practice webscrape/jj.py
@@ -13,3 +13,7 @@ main = [wrapper.select('main.page-main')for wrapper in page_wrapper]
 for columns in main:
     for column in columns:
         complete_items = column.select('div.columns div.column.main div.products.wrapper.grid.products-grid ol li.item.product.product-item')
+
+product_names_id = []
+for items in complete_items:
+    product_names_id.append(items.select('div.product-item-info div.product.details.product-item-details h2.product.name.product-item-name a'))
