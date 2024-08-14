eed384f1275d35304f6e1230ef800a4941b0547b
muhammadhananbaloch <106446229+muhammadhananbaloch@users.noreply.github.com>
2024-08-14 22:12:26 +0500

    Scrap all books info.

--- Web Scraping/practice webscrape/bookstoscrape/bookstoscrape.py
+++ Web Scraping/practice webscrape/bookstoscrape/bookstoscrape.py
@@ -9,4 +9,27 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 books = soup.find_all('article', class_='product_pod')
 
+book_title= []
+book_price = []
+book_rating = []
+stock_status = []
+for book in books:
+    try:
+        book_title.append(book.h3.a['title'])
+    except:
+        book_title.append('No title available')
+    
+    try:
+        book_price.append(book.find('p', class_='price_color').text.strip('Â£'))
+    except:
+        book_price('Price not available')
+    try:
+        book_rating.append(book.p['class'][1])  
+    except:
+        book_rating.append('Rating not available')
+
+    try:
+        stock_status.append(book.find('p', class_='instock availability').text.strip())
+    except:
+        stock_status.append('Stock status not available')
 
