eb5fcb7722fa4db1ca70b55c93fbc62f5c4d76a9
muhammadhananbaloch <106446229+muhammadhananbaloch@users.noreply.github.com>
2024-08-18 13:39:58 +0500

    Locate a single element using selenium driver from webpage.

---
+++ Web Scraping/Basics(Selenium)/locating_single_element.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+
+driver = webdriver.Chrome()
+query = 'laptop'
+driver.get(f"https://www.amazon.com/s?k={query}&ref=cs_503_search")
+time.sleep(15)
+elem =  driver.find_element(By.CLASS_NAME, 'puis-card-container')
+print(elem.text)
+
+driver.close()
