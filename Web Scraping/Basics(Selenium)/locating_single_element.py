ca74ee5a8857599e7518d105c5fd47240e5b4bc8
muhammadhananbaloch <106446229+muhammadhananbaloch@users.noreply.github.com>
2024-08-18 13:52:14 +0500

    Get outer html of given element.

--- Web Scraping/Basics(Selenium)/locating_single_element.py
+++ Web Scraping/Basics(Selenium)/locating_single_element.py
@@ -8,6 +8,7 @@ query = 'laptop'
 driver.get(f"https://www.amazon.com/s?k={query}&ref=cs_503_search")
 time.sleep(15)
 elem =  driver.find_element(By.CLASS_NAME, 'puis-card-container')
-print(elem.text)
+# print(elem.text)
+print(elem.get_attribute('outerHTML'))
 
 driver.close()
