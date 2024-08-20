0c19985277dc1c05e9e93f085a639be367996c2d
muhammadhananbaloch <106446229+muhammadhananbaloch@users.noreply.github.com>
2024-08-20 16:58:43 +0500

    Add input options to make it interactive.

--- Web Scraping/Basics(Selenium)/dynamic_site.py
+++ Web Scraping/Basics(Selenium)/dynamic_site.py
@@ -20,14 +20,40 @@ try:
     all_matches_button.click() # Click the button to show all matches
     
     dropdown_country = Select(driver.find_element(By.ID, 'country'))    # Find the dropdown menu to select the country
-    dropdown_country.select_by_visible_text('Spain')    # Select England from the dropdown menu
+    print(f"Available countries in list: {[options.text for options in dropdown_country.options]}")    # Display the options in the dropdown menu
+    try:
+        country = input("Enter a country: ")    # Prompt the user to enter a country
+        dropdown_country.select_by_visible_text(str(country).capitalize())    # Select England from the dropdown menu
+    except:
+        driver.quit()
+        print("Invalid input. Please try again.")
+    
+    time.sleep(2)    # Wait for the page to load
+
+
 
     dropdown_league = Select(driver.find_element(By.ID, 'league'))
-    dropdown_league.select_by_visible_text('La Liga')    # Select the Premier League from the dropdown menu 
+    print(f"Available leagues in list: {[options.text for options in dropdown_league.options]}")    # Display the options in the dropdown menu
+    try:
+        league = input("Enter a league: ")    # Prompt the user to enter a league
+        dropdown_league.select_by_visible_text(' '.join(str(word).capitalize() for word in league.split()))    # Select the Premier League from the dropdown menu 
+    except:
+        driver.quit()
+        print("Invalid input. Please try again.")
+
+    time.sleep(2)    # Wait for the page to load
+
+
 
     dropdown_season = Select(driver.find_element(By.ID, 'season'))    # Find the dropdown menu to select the season 
-    dropdown_season.select_by_visible_text("22/23") 
-
+    print(f"Available seasons in list: {[options.text for options in dropdown_season.options]}")    # Display the options in the dropdown menu
+    try:
+        season = input("Enter a season: ")    # Prompt the user to enter a season
+        dropdown_season.select_by_visible_text(str(season))    # Select the 22/23 season from the dropdown menu
+    except:
+        driver.quit()
+        print("Invalid input. Please try again.")
+    
     time.sleep(2)
     matches = driver.find_elements(By.TAG_NAME, "tr") # Click the button to show all matches
 
@@ -51,7 +77,7 @@ try:
         try:
             away_team.append(match.find_element(By.XPATH, "./td[4]").text)
         except: 
-            away_team.append("Not found")  
+            away_team.append("Not found")
     # time.sleep(10)   
     driver.quit()
 except Exception as e:
