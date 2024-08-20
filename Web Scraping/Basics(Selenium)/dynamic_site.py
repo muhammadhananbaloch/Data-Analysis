dc583bd0f36193fa8f4b33d75e73315a14947b2c
muhammadhananbaloch <106446229+muhammadhananbaloch@users.noreply.github.com>
2024-08-20 14:48:46 +0500

    Choose options from select button an scrap further data.

--- Web Scraping/Basics(Selenium)/dynamic_site.py
+++ Web Scraping/Basics(Selenium)/dynamic_site.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By 
+from selenium.webdriver.support.ui import Select
 import time
 import pandas as pd
 
@@ -17,12 +18,16 @@ try:
 
     all_matches_button = driver.find_element(By.XPATH, "//label[@analytics-event='All matches']")    # Find the button to show all matches  
     all_matches_button.click() # Click the button to show all matches
+    
+    dropdown_country = Select(driver.find_element(By.ID, 'country'))    # Find the dropdown menu to select the country
+    dropdown_country.select_by_visible_text('Spain')    # Select England from the dropdown menu
 
-    # select_season_button = driver.find_element(By.XPATH, '//*[@id="season"]')    # Find the button to show all seasons
-    # select_season_button.click() # Click the button to show all seasons
+    dropdown_league = Select(driver.find_element(By.ID, 'league'))
+    dropdown_league.select_by_visible_text('La Liga')    # Select the Premier League from the dropdown menu 
 
-    choose_season_button = driver.find_element(By.XPATH, '//*[@label="21/22"]')    # Find the button to show the 21/22 season
-    choose_season_button.click() # Click the button to show the 21/22 season
+    dropdown_season = Select(driver.find_element(By.ID, 'season'))    # Find the dropdown menu to select the season 
+    dropdown_season.select_by_visible_text("22/23") 
+
     time.sleep(2)
     matches = driver.find_elements(By.TAG_NAME, "tr") # Click the button to show all matches
 
@@ -31,12 +36,22 @@ try:
     away_team = []
     score = []
     for match in matches:
-        date.append(match.find_element(By.XPATH, "./td[1]").text)
-        home = match.find_element(By.XPATH, "./td[2]").text
-        home_team.append(home)  
-        print(home)
-        score.append(match.find_element(By.XPATH, "./td[3]").text)
-        away_team.append(match.find_element(By.XPATH, "./td[4]").text)
+        try:
+            date.append(match.find_element(By.XPATH, "./td[1]").text)
+        except:
+            date.append("Not found")
+        try:
+            home_team.append(match.find_element(By.XPATH, "./td[2]").text)
+        except:
+            home_team.append("Not found")
+        try:
+            score.append(match.find_element(By.XPATH, "./td[3]").text)
+        except:
+            score.append("Not found")
+        try:
+            away_team.append(match.find_element(By.XPATH, "./td[4]").text)
+        except: 
+            away_team.append("Not found")  
     # time.sleep(10)   
     driver.quit()
 except Exception as e:
